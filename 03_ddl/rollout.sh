cc7dba24e772542b2fcd61ca3ef895d2c904f804
azarmike <31337mike@gmail.com>
2025-12-19 10:34:04 +0300

    Update rollout.sh

--- 03_ddl/rollout.sh
+++ 03_ddl/rollout.sh
@@ -95,6 +95,21 @@ if [ "$filter" == "gpdb" ]; then
 
 				counter=$(($counter + 1))
 			done
+		elif [ "$VERSION" == "gpdb_7" ]; then
+			for x in $(psql -v ON_ERROR_STOP=1 -q -A -t -c "select rank() over(partition by g.hostname order by g.datadir), g.hostname from gp_segment_configuration g where g.content >= 0 and g.role = 'p' order by g.hostname"); do
+				CHILD=$(echo $x | awk -F '|' '{print $1}')
+				EXT_HOST=$(echo $x | awk -F '|' '{print $2}')
+				PORT=$(($GPFDIST_PORT + $CHILD))
+
+				if [ "$counter" -eq "0" ]; then
+					LOCATION="'"
+				else
+					LOCATION+="', '"
+				fi
+				LOCATION+="gpfdist://$EXT_HOST:$PORT/"$table_name"_[0-9]*_[0-9]*.dat"
+
+				counter=$(($counter + 1))
+			done
 		else
 			for x in $(psql -v ON_ERROR_STOP=1 -q -A -t -c "select rank() over (partition by g.hostname order by p.fselocation), g.hostname from gp_segment_configuration g join pg_filespace_entry p on g.dbid = p.fsedbid join pg_tablespace t on t.spcfsoid = p.fsefsoid where g.content >= 0 and g.role = 'p' and t.spcname = 'pg_default' order by g.hostname"); do
 				CHILD=$(echo $x | awk -F '|' '{print $1}')
